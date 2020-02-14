a5a793e1926841aa2b917deecc4991d5fdae1253
root <root@ip-172-31-8-151.us-west-1.compute.internal>
2020-02-14 05:05:21 +0000

    echo command

---
+++ echo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Basic shell scripts
+
+echo -e "Enter your name:\n"        # -e options enable '\n' operation. Without -e, echo will print \n.
+read user
+
+sleep 2 
+echo "Hello $user"
+ 
+sleep 2
+echo "Learn more about shell scripting "
+echo "Good Luck!!"
+
