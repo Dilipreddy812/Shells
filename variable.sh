0b87101c1d929eb2c9431533fc5fe578444c85a8
root <root@station87.example.com>
2020-02-14 20:13:28 +0530

    Uses of variable & echo & read & sleep

---
+++ variable.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#Variable assign and output shows with help of echo
+#In this scenario we have to assign variable value before calling.
+#Normal variable assign
+_student="Rahul"
+
+echo "$_student Congratulations!!!"
+
+echo "#################################"
+sleep 2
+
+my_val=500
+echo "You score is $my_val."
+
+echo "#################################"
+#variable is assign after calling...
+#It will print blank.
+echo "$set"
+
+set="codes"
+
+
+
+
