3ae21604740484d8195da9daa25634c1e4b0422a
root <root@station87.example.com>
2020-02-14 20:12:09 +0530

    basics uses of echo

---
+++ basic.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Between double quotes or single quotes. It will print the same.
+
+echo "Hello   Learner"     #It will print msg with given spaces.
+
+echo "Hello Learner"       #It will print the same.
+
+echo "Hello * Learner"    #It will print the same.
+
+echo 'Hello Learner * Hello Learner'
+
+echo Hello * Learner     #It will print the content of the present directory.Ex: Hello content Learner
+
+echo "Hello "*" Learner"    #It will print Hello * Learner.Because double quotes hold it * at this time.
+
+echo `command` learner      # between this  `  ` must be a command.
+
