6ecf657193303bc7889cd83b5185db8ad56a0514
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2020-02-15 10:15:56 +0530

    Update the comment
    Update the error

--- basic.sh
+++ basic.sh
@@ -7,11 +7,11 @@ echo "Hello Learner"       #It will print the same.
 
 echo "Hello * Learner"    #It will print the same.
 
-echo 'Hello Learner * Hello Learner'
+echo 'Hello Learner * Hello Learner'       #It will print the same.
 
 echo Hello * Learner     #It will print the content of the present directory.Ex: Hello content Learner
 
-echo "Hello "*" Learner"    #It will print Hello * Learner.Because double quotes hold it * at this time.
+echo "Hello "*" Learner"    #It will print Hello * Learner.Because double quotes hold it * (asterisk) at this time.
 
-echo `command` learner      # between this  `  ` must be a command.
+echo `command` learner      # between this  `---`(back-tic) must be a command.
 
