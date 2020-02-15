cc3dc161bc05b26d964366f0fcce0733a5b51fc8
root <root@ip-172-31-22-112.ap-southeast-1.compute.internal>
2020-02-15 08:19:06 +0000

    added more sections

--- for_case.sh
+++ for_case.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #use case of 'for' && 'if..then..elif..then..else..fi'in shell.
-
+# -r --> file has read permission, -w --> file has write permission, -x --> file has execute permission.
 echo "Type file name to create: "
 read name
 for i in $name
 do
-	if [ -e $name ]		#-e check file is exists or not:
+	if [ -e $name ]		#-e check file is exists or not.
 	then
 	    echo "Already exists"
-	elif [ -f $name ]
+	elif [ -f $name ]       #checks file is a ordinary file. 
 	then
 	    echo "Already exists"
         else
