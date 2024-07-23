4a81b8dcbcb47f20f581826e5c08818802a36c11
dilip reddy <83334228+Dilipreddy812@users.noreply.github.com>
2024-07-23 12:36:11 +0530

    Update and rename array.sh to chromium.sh

--- array.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -d --name chromium --security-opt seccomp=unconfined -e PUID=1000 -e PGID=1000 -e TZ=Etc/UTC -e CHROME_CLI=https://www.linuxserver.io -v /path/to/config:/config -p 3000:3000 -p 3001:3001 --shm-size=1gb --restart unless-stopped lscr.io/linuxserver/chromium:latest
