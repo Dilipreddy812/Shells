8951db107110b50da0494dd5169a0c6781113e60
dilip reddy <83334228+Dilipreddy812@users.noreply.github.com>
2024-07-23 12:29:00 +0530

    Update array.sh

--- array.sh
+++ array.sh
@@ -1,34 +1,3 @@
 #!/bin/bash
 
-# Set the Docker image and container name
-IMAGE=lscr.io/linuxserver/chromium:latest
-CONTAINER_NAME=chromium
-
-# Set the security options
-SECURITY_OPTS=(-seccomp=unconfined)
-
-# Set the environment variables
-ENV_VARS=(-e PUID=1000 -e PGID=1000 -e TZ=Etc/UTC -e CHROME_CLI=https://www.linuxserver.io)
-
-# Set the volume mount
-VOLUME_MOUNTS=(-v /path/to/config:/config)
-
-# Set the port mappings
-PORT_MAPPINGS=(-p 3000:3000 -p 3001:3001)
-
-# Set the shared memory size
-SHM_SIZE=1gb
-
-# Set the restart policy
-RESTART_POLICY=unless-stopped
-
-# Run the Docker command
-docker run -d \
-  --name ${CONTAINER_NAME} \
-  ${SECURITY_OPTS[@]} \
-  ${ENV_VARS[@]} \
-  ${VOLUME_MOUNTS[@]} \
-  ${PORT_MAPPINGS[@]} \
-  --shm-size=${SHM_SIZE} \
-  --restart=${RESTART_POLICY} \
-  ${IMAGE}
+docker run -d --name chromium --security-opt seccomp=unconfined -e PUID=1000 -e PGID=1000 -e TZ=Etc/UTC -e CHROME_CLI=https://www.linuxserver.io -v /path/to/config:/config -p 3000:3000 -p 3001:3001 --shm-size=1gb --restart unless-stopped lscr.io/linuxserver/chromium:latest
